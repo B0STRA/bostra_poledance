bad976db29b0f0cbc5350dbde3b27a329e46be1b
Bostra <119994243+B0STRA@users.noreply.github.com>
2024-04-27 13:00:11 -0400

    Oops. Forgot to update config 🫠

--- config/config.lua
+++ config/config.lua
@@ -1,9 +1,24 @@
-Config = { Debug = false, UseModels = true, Target = 'lib' } --UseModels true for all prop instances, Target 'qb' or 'ox' or 'lib'
+return {
+    Debug = false,
+    UseModels = false,
+    Target = 'qb',                                        --UseModels true for all prop instances, Target 'qb' or 'ox' or 'lib'
+    Poles = {
+        { position = vec4(-1388.7698974609, -674.28186035156, 27.856121063232, 0.0), spawn = true },
+        --position vec4, spawn optional, job optional
+        { position = vector4(116.66, -1292.19, 29.26, 30),                        job = '' },
+        { position = vector4(111.50, -1295.17, 29.26, 30),                        job = '' },
+        { position = vector4(127.195, -1294.079, 21.69, 30),                      job = '' },
+        { position = vector4(116.336, -1300.352, 21.69, 30),                      job = '' },
+        { position = vector4(109.549, -1288.595, 21.69, 30),                      job = '' },
+        { position = vector4(120.391, -1282.339, 21.69, 30),                      job = '' },
+        { position = vector4(114.499, -1277.792, 21.387, 30),                     job = '' },
+        { position = vector4(108.674, -1281.162, 21.387, 30),                     job = '' },
+        { position = vector4(128.152, -1301.439, 21.387, 30),                     job = '' },
+        { position = vector4(122.327, -1304.808, 21.387, 30),                     job = '' },
+        { position = vector4(106.7449, -1313.185, 29.84712, 30),                  job = '' },
+        { position = vector4(109.5838, -1311.546, 29.84712, 30),                  job = '' },
+        { position = vector4(112.2671, -1309.997, 29.84712, 30),                  job = '' },
+        { position = vec4(121.75637817383, -1289.525390625, 28.218925476074, 0.0), job = '' },
+    }
 
-Config.Poles = {
-    { position = vec4(104.07, -1292.23, 28.26, 301.77),                  spawn = true }, --position vec4, spawn optional, job optional
-    -- Vanilla VU Poles
-    { position = vector4(104.16384124756, -1294.2568359375, 28.26, 30),  job = 'unicorn' },
-    { position = vector4(102.25046539307, -1290.8802490234, 28.25, 30),  job = 'unicorn' },
-    { position = vector4(112.580322265631, -1287.0412597656, 27.46, 30), job = 'unicorn' }
 }
